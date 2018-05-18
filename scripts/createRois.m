a08ba4d72d10390c162f0c47bcfd3bdb72c9bbad
simon <schw4b@gmail.com>
2018-05-18 15:18:17 +0100

    created thalamocort atlas

--- scripts/createRois.m
+++ scripts/createRois.m
@@ -39,3 +39,33 @@ save_nii(yeo, [PATH 'atlas/yeo/ass_' file])
 % labels
 load ([PATH 'atlas/yeo/scripts/17NetworksNetworkNames.mat'])
 UniqueNetName
+
+
+%% Prepare thalcort atlas
+PATH_ATLAS = '~/Data/ThalCortCon/atlas/';
+
+% Load atlas
+nii_thal = load_nii([PATH_ATLAS 'thalamus/rCoreg_NN_Thalamus-maxprob-thr25-1mm.nii.gz']);
+%nii_cort = load_nii([PATH_ATLAS 'yeo/r16mmCoregResliceNearestN_ass_Yeo2011_7Networks_N1000.split_components.FSL_MNI152_FreeSurferConformed_1mm.nii.gz']);
+nii_cort = load_nii([PATH_ATLAS 'yeo/rCoregNNYeo2011_17Networks_N1000.split_components.FSL_MNI152_FreeSurferConformed_1mm.nii.gz']);
+% increase labels
+nr=max(nii_thal.img(:));
+nii_cort.img(nii_cort.img==0) = NaN;
+nii_cort.img = nii_cort.img + single(nr);
+nii_cort.img(isnan(nii_cort.img)) = 0;
+
+%min(nii_cort.img(:))
+%max(nii_cort.img(:))
+
+% merge the two atlases
+new=nii_cort;
+new.img = uint8(nii_cort.img) + nii_thal.img;
+
+min(new.img(:))
+max(new.img(:))
+
+save_nii(new, [PATH_ATLAS 'thalcort/r16mm_Thal07_Cort114.nii'])
+
+%% sanity check
+nii = load_nii([PATH_ATLAS 'thalcort/r16mm_Thal07_Cort114.nii']);
+length(unique(nii.img(:)))-1
