2a8c91ce6a4eb0dbecfbb3b06e55e001fed61998
simon <schw4b@gmail.com>
2018-05-18 13:27:04 +0100

    Yeo node assigned

--- scripts/createRois.m
+++ scripts/createRois.m
@@ -3,7 +3,7 @@
 PATH = '~/Data/ThalCortCon/';
 
 %% Split thalamus load mask into single binary 3D images
-mask = load_nii([PATH 'thalamus/rCoreg_NN_Thalamus-maxprob-thr25-1mm.nii.gz']);
+mask = load_nii([PATH 'atlas/thalamus/rCoreg_NN_Thalamus-maxprob-thr25-1mm.nii.gz']);
 
 fid= fopen([PATH 'rois/labels_thalamus.txt']);
 labels = textscan(fid, '%d %s');
@@ -18,3 +18,24 @@ for n=1:Nn
 end
 
 
+%% Prepare Yeo atlas
+PATH_YEO = '~/Drive/yeo/Yeo_JNeurophysiol11_SplitLabels/MNI152/';
+file = 'Yeo2011_17Networks_N1000.split_components.FSL_MNI152_FreeSurferConformed_1mm.nii';
+
+yeo = load_nii([PATH_YEO file]);
+
+load ([PATH 'atlas/yeo/scripts/17NetworksNodeAssigments.mat'])
+Nn = length(NodeAss2Nets_Idx);
+new = yeo.img;
+
+for i=1:Nn
+    new(yeo.img == i) = NodeAss2Nets_Idx(i);
+end
+yeo.img=new;
+
+max(yeo.img(:))
+save_nii(yeo, [PATH 'atlas/yeo/ass_' file])
+
+% labels
+load ([PATH 'atlas/yeo/scripts/17NetworksNetworkNames.mat'])
+UniqueNetName
