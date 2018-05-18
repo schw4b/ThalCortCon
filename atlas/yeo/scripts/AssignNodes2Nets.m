5d3ac9bda8d1f148f782c2fd5a97784d066861d7
asoroosh <srafyouni@gmail.com>
2018-05-18 10:15:28 +0100

    NNfixed

--- atlas/yeo/scripts/AssignNodes2Nets.m
+++ atlas/yeo/scripts/AssignNodes2Nets.m
@@ -13,9 +13,10 @@ fclose(fileID);
 NetNamesList = [dataArray{1:end-1}];
 clearvars filename startRow formatSpec fileID dataArray ans
 
-disp(['There are: ' num2str(size(NetNamesList,1)) ' nodes.'])
+NN = size(NetNamesList,1);
+disp(['There are: ' num2str(NN) ' nodes.'])
 
-for i = 1:size(NetNamesList,1)
+for i = 1:NN
     C = strsplit(NetNamesList{i},'_');
     NodeAss2Nets_LHRH{i} = [C{2} '_' C{3}]; 
     NodeAss2Nets{i} = [C{3}]; 
@@ -23,7 +24,7 @@ end
 
 UniqueNetName = unique(NodeAss2Nets);
 
-NodeAss2Nets_Idx = zeros(1,114);
+NodeAss2Nets_Idx = zeros(1,NN);
 for n = 1:numel(UniqueNetName)
      IDX_tmp = find(~cellfun(@isempty,strfind(NetNamesList,UniqueNetName{n})));
      NodeAss2Nets_Idx(IDX_tmp) = n;
